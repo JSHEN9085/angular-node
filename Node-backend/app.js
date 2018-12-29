c7114a77986ba7c658b0182aadcd62f80e014d35
Jun Jian Shen <jshen9085@gmail.com>
2018-12-29 11:27:21 -0500

    refactor backend code

--- Node-backend/app.js
+++ Node-backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
-const Post = require('./models/post')
+const postsRoutes = require('./routes/posts')
 // app.use((req, res, next) => {
 //   console.log('Fist middleware');
 //   next(); // continue to response, if we comment it, it will stay in here forever
@@ -19,57 +19,9 @@ app.use((req, res, next) => {
   next()
 })
 
-
-app.get('/api/posts' , (req, res, next) => {
-  Post.find().then(documents => {
-    res.status(200).json({
-      posts: documents
-    });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({message: "Post is not found"})
-    }
-  })
-})
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(result => {
-    res.status(201).json({
-      message: "Post added",
-      postId: result._id
-    });
-  })
-});
-
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  })
-  Post.updateOne({_id: req.params.id}, post).then(result => {
-    res.status(200).json({message: "successfully updated"})
-  })
-})
-
-app.delete('/api/posts/:id', (req, res, next) => {
-  // console.log(req.params.id);
-  Post.deleteOne({_id: req.params.id}).then(result => {
-    res.status(200).json({message: "Post deleted"})
-  })
-})
+app.use('/api/posts', postsRoutes);
 
 module.exports = app;
