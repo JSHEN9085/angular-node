4be0ee5727ff419d4e38f96a512e3aa1c30d6f35
Jun Jian Shen <jshen9085@gmail.com>
2019-01-07 11:39:34 -0500

    added sign up form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  isLoading = true;
-  
+  isLoading = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onLogin(form: NgForm) {
+    console.log(form.value)
   }
 
 }
